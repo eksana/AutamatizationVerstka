c11cd02945b896e58a5333f7c23428a3af6054bf
eksana <ser.vajnrub@yandex.ru>
2017-03-09 17:30:58 +0300

    create files for scss

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp      = require('gulp'), // Подключаем Gulp
+    sass        = require('gulp-sass'), //Подключаем Sass пакет
+     autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автомат
+
+    gulp.task('sass-ui', function(){ // Создаем таск Sass
+    return gulp.src('src_ui/sass/**/*.scss') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
+        .pipe(gulp.dest('src_ui/css-ui')) // Выгружаем результата в папку app/css
+        
+});
+    /*gulp.task('mytask', function() {
+    console.log('Привет, я таск!');
+});*/
